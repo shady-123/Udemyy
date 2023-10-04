a4bcf5b4c22bf2a81c40034d373928532c15a369
Sharad Kumar <sharad123@gmail.com>
2023-10-04 13:24:41 +0530

    SK's new commit

--- src/com/sapient/hybris/ui/ShapeDemo.java
+++ src/com/sapient/hybris/ui/ShapeDemo.java
@@ -9,14 +9,17 @@ public class ShapeDemo {
 
 	public static void main(String[] args) {
 		//Array of references of Shape type
-		Shape shapes[]= new Shape[5];
+		Shape shapes[]= new Shape[8];
 		
 		shapes[0]= new Circle(5.0);
 		shapes[1]= new Rectangle(10.0,5.0);
 		shapes[2]= new Rectangle(5.0,5.0);
 		shapes[3]= new Circle(10.0);
 		shapes[4]= new Triangle(8.0,7.5,8.2);
-		
+		shapes[5]= new Circle(12.0);
+		shapes[6]= new Rectangle(5.0,6.0);
+		shapes[7]= new Triangle(2.0,2.0,2.0);
+
 		for(Shape s: shapes) {
 			if(s instanceof Circle) {
 				System.out.println("Area of circle: "+ s.computeArea());
